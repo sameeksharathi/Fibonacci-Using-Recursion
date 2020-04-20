39dcb3cdf21a1894fb50e5fbb775654264cb4b5b
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 20:00:53 +0530

    Fibbonacci series class is created

---
+++ FibonacciUsingRecursion.java
@@ -0,0 +1,13 @@
+public class FibbonacciUsingRecursion {
+    public int fib(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        else{
+            return fib(n-2)+fib(n-1);
+        }
+    }
+}
